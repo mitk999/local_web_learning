9a60a76a7dbc0f04d851f7bd43581b134dfd85f2
t.m <mieno.tk0909@gmail.com>
2024-04-18 22:51:58 +0900

    selection css update. to center

--- study-selection/script.js
+++ study-selection/script.js
@@ -10,6 +10,7 @@ let currentQuestionIndex = 0;
 let score = 0;
 let questions = [];
 
+
 fileUpload.addEventListener("change", function(event) {
     const file = event.target.files[0];
     if (file) {
@@ -17,7 +18,7 @@ fileUpload.addEventListener("change", function(event) {
         reader.onload = function(e) {
             const fileContent = e.target.result;
             try {
-                questions = JSON.parse(fileContent);
+                questions = shuffle_arr(JSON.parse(fileContent));
                 currentQuestionIndex = 0;
                 score = 0;
                 showQuestion(questions[currentQuestionIndex]);
@@ -36,7 +37,9 @@ function showQuestion(question) {
     let uttr = new SpeechSynthesisUtterance(question.word)
     uttr.lang = "en-US";
     speechSynthesis.speak(uttr)
-
+    if (question.options_shuffle){
+        question.options = shuffle_arr(question.options)
+    }
     question.options.forEach(option => {
         const button = document.createElement("button");
         button.textContent = option;
@@ -61,7 +64,7 @@ function checkAnswer(selectedOption) {
         resultElement.style.color = "red";
     }
 
-    scoreElement.textContent = `今${currentQuestionIndex + 1}問中${score}問正解`;
+    scoreElement.textContent = `正解：${score}/${currentQuestionIndex + 1}`;
 
     resultElement.style.display = "block";
 
@@ -72,15 +75,12 @@ function checkAnswer(selectedOption) {
             showQuestion(questions[currentQuestionIndex]);
         } else {
             const logMessage = `正解率: ${(score / questions.length) * 100}%`;
-            // Blobを作成
             const blob = new Blob([logMessage], { type: "text/plain" });
-            // ダウンロードリンクを作成
             const downloadLink = document.createElement("a");
             downloadLink.href = URL.createObjectURL(blob);
             downloadLink.download = "quiz_log.txt";
-            // ダウンロードリンクをクリック
             downloadLink.click();
-            alert("全ての問題が終了しました。");
+            alert("全ての問題が終了しました");
             currentQuestionIndex = 0;
             score = 0;
             showQuestion(questions[currentQuestionIndex]);
@@ -88,3 +88,14 @@ function checkAnswer(selectedOption) {
     }, 500);
 }
 
+function shuffle_arr(array) {
+  let currentIndex = array.length;
+
+  while (currentIndex != 0) {
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array
+}
