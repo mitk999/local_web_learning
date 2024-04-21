f0478bc2d30e60e8fb6b7dac7718dbe9ece75cd1
t.m <mieno.tk0909@gmail.com>
2024-04-21 22:12:01 +0900

    count word recursive

--- managing_jobs/windows_wordcount.bat
+++ managing_jobs/windows_wordcount.bat
@@ -17,12 +17,14 @@ if not exist "output" mkdir "output"
 REM 各 "word" の出現回数を格納する連想配列を初期化
 set "word_counts="
 
-REM フォルダ内のJSONファイルを走査
-for %%f in ("%folder_path%\*.json") do (
+REM 再帰的にフォルダを検索するサブルーチン
+:searchFolders
+for /r "%folder_path%" %%F in (*.json) do (
     REM JSONファイルから各 "word" の出現回数を取得
-    set "json_file=%%f"
-    for /f "tokens=2 delims=:,{}" %%w in ('type "!json_file!" ^| findstr /r /c:"\"word\":"') do (
-        set "word=%%w"
+    set "json_file=%%F"
+    set "folder_path=%%~dpF"
+    for /f "tokens=2 delims=:,{}" %%W in ('type "!json_file!" ^| findstr /r /c:"\"word\":"') do (
+        set "word=%%W"
         REM 両端のクォーテーションを削除
         set "word=!word:~1,-1!"
         REM 出現回数をカウント
@@ -33,9 +35,10 @@ for %%f in ("%folder_path%\*.json") do (
 REM 結果を "word_count.txt" として出力
 set output_file="output\word_count.txt"
 (
+    echo フォルダ: %folder_path%
     echo 各 "word" の出現回数:
-    for /f "tokens=2,* delims=[]=" %%i in ('set word_counts[') do (
-        echo %%i: %%j
+    for /f "tokens=2,* delims=[]=" %%I in ('set word_counts[') do (
+        echo %%I: %%J
     )
 ) > %output_file%
 
